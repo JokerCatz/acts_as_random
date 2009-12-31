b43dbe388f1c3fa57b136886a3f2e4e520227981
JokerCatz <lovingcatz@gmail.com>
2009-12-31 13:41:17 +0800

    fix bug

--- lib/active_record/acts/random.rb
+++ lib/active_record/acts/random.rb
@@ -8,10 +8,11 @@ module ActiveRecord
         def acts_as_random(options = {})
           configuration={:count => 3}
           configuration.update(options) if options.is_a?(Hash)
+          class_eval <<-EOV
             def self.pick(conditions = "true")
               temp_class = has_parent_class?
-              return self.find_by_sql("SELECT * FROM #{self.table_name} as r1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM #{self.table_name})) AS gid) AS r2 WHERE r1.id >= r2.gid AND #{conditions} ORDER BY r1.id ASC LIMIT 1;").first unless temp_class
-              return self.find_by_sql("SELECT * FROM #{temp_class.table_name} as r1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM #{temp_class.table_name})) AS gid) AS r2 WHERE r1.id >= r2.gid AND type = '#{self.to_s}' AND #{conditions} ORDER BY r1.id ASC LIMIT 1;").first
+              return self.find_by_sql("SELECT * FROM \#{self.table_name} as r1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM \#{self.table_name})) AS gid) AS r2 WHERE r1.id >= r2.gid AND \#{conditions} ORDER BY r1.id ASC LIMIT 1;").first unless temp_class
+              return self.find_by_sql("SELECT * FROM \#{temp_class.table_name} as r1 JOIN (SELECT CEIL(RAND() * (SELECT MAX(id) FROM \#{temp_class.table_name})) AS gid) AS r2 WHERE r1.id >= r2.gid AND type = '\#{self.to_s}' AND \#{conditions} ORDER BY r1.id ASC LIMIT 1;").first
             end
             def self.picks(limit = configuration[:count] , conditions = "true")
               temp = []
@@ -28,6 +29,7 @@ module ActiveRecord
               end
               return false
             end
+          EOV
         end
       end
     end
